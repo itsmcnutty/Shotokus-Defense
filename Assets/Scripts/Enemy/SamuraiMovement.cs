9e66de11f95a0273fcb552ce4ae7a8937f98b420
wblucca <wblucca@wpi.edu>
2019-07-25 11:11:36 +0900

    Add samurai dummy that will follow player's head slowly

---
+++ Assets/Scripts/Enemy/SamuraiMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamuraiMovement : MonoBehaviour
+{
+
+    public const float SPEED = 0.001f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        Vector3 move = SPEED * (transform.position - playerTransform.position).normalized;
+        
+        transform.Translate(move);
+    }
+}
