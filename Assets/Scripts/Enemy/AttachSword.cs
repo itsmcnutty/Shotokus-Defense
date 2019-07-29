565fb7d2c051a33761cf49199ef6dac90b028904
wblucca <wblucca@wpi.edu>
2019-07-29 09:28:54 +0900

    Test sword attachment

--- Assets/Scripts/Enemy/AttachSword.cs
+++ Assets/Scripts/Enemy/AttachSword.cs
@@ -9,7 +9,11 @@ public class AttachSword : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.parent = transform.parent.transform.Find("hand.R").transform;
+        Transform handBone = transform.parent.transform.Find(
+            "free3dmodel_skeleton/hips/abdomen/abdomen2/chest/shoulder.R/upper_arm.R/forearm.R/hand.R");
+        
+        // Make sword child of the hand bone
+        transform.parent = handBone.transform;
     }
 
     // Update is called once per frame
