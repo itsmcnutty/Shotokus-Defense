1f073f3236abfeabeb09aacdcfe7a38233821ad9
itsmcnutty <epplevinsky@wpi.edu>
2019-07-30 16:30:33 +0900

    Final removals made

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,331 +1,280 @@
-﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
-//
-// Purpose: Handles all the teleport logic
-//
-//=============================================================================
-
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
-namespace Valve.VR.InteractionSystem
+public class Test : MonoBehaviour
 {
-	//-------------------------------------------------------------------------
-	public class Test : MonoBehaviour
+	public SteamVR_Action_Boolean teleportAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean> ("Teleport");
+
+	public LayerMask traceLayerMask;
+	public Transform destinationReticleTransform;
+	public Transform invalidReticleTransform;
+	public Color pointerValidColor;
+	public Color pointerInvalidColor;
+	public Color pointerLockedColor;
+
+	public float arcDistance = 10.0f;
+
+	private LineRenderer pointerLineRenderer;
+	private Hand pointerHand = null;
+	private Player player = null;
+	private TeleportArc teleportArc = null;
+
+	private bool visible = false;
+
+	private Vector3 pointedAtPosition;
+
+	private float invalidReticleMinScale = 0.2f;
+	private float invalidReticleMaxScale = 1.0f;
+	private float invalidReticleMinScaleDistance = 0.4f;
+	private float invalidReticleMaxScaleDistance = 2.0f;
+	private Vector3 invalidReticleScale = Vector3.one;
+	private Quaternion invalidReticleTargetRotation = Quaternion.identity;
+
+	private static Test _instance;
+	public static Test instance
 	{
-		public SteamVR_Action_Boolean teleportAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean> ("Teleport");
-
-		public LayerMask traceLayerMask;
-		public Transform destinationReticleTransform;
-		public Transform invalidReticleTransform;
-		public Color pointerValidColor;
-		public Color pointerInvalidColor;
-		public Color pointerLockedColor;
-
-		public float arcDistance = 10.0f;
-
-		private LineRenderer pointerLineRenderer;
-		private Hand pointerHand = null;
-		private Player player = null;
-		private TeleportArc teleportArc = null;
-
-		private bool visible = false;
-
-		private Vector3 pointedAtPosition;
-		
-		private float invalidReticleMinScale = 0.2f;
-		private float invalidReticleMaxScale = 1.0f;
-		private float invalidReticleMinScaleDistance = 0.4f;
-		private float invalidReticleMaxScaleDistance = 2.0f;
-		private Vector3 invalidReticleScale = Vector3.one;
-		private Quaternion invalidReticleTargetRotation = Quaternion.identity;
-
-		//-------------------------------------------------
-		private static Test _instance;
-		public static Test instance
+		get
 		{
-			get
+			if (_instance == null)
 			{
-				if (_instance == null)
-				{
-					_instance = GameObject.FindObjectOfType<Test> ();
-				}
+				_instance = GameObject.FindObjectOfType<Test> ();
+			}
 
-				return _instance;
+			return _instance;
+		}
+	}
+
+	void Awake ()
+	{
+		_instance = this;
+
+		pointerLineRenderer = GetComponentInChildren<LineRenderer> ();
+
+		teleportArc = GetComponent<TeleportArc> ();
+		teleportArc.traceLayerMask = traceLayerMask;
+
+		float invalidReticleStartingScale = invalidReticleTransform.localScale.x;
+		invalidReticleMinScale *= invalidReticleStartingScale;
+		invalidReticleMaxScale *= invalidReticleStartingScale;
+	}
+
+	void Start ()
+	{
+		HidePointer ();
+
+
+		if (player == null)
+		{
+			Destroy (this.gameObject);
+		}
+	}
+
+	void Update ()
+	{
+		Hand newPointerHand = null;
+
+		foreach (Hand hand in player.hands)
+		{
+			if (visible)
+			{
+				if (WasTeleportButtonReleased (hand))
+				{ }
+			}
+
+			if (WasTeleportButtonPressed (hand))
+			{
+				newPointerHand = hand;
 			}
 		}
 
-		//-------------------------------------------------
-		void Awake ()
+		if (!visible && newPointerHand != null)
 		{
-			_instance = this;
-
-			pointerLineRenderer = GetComponentInChildren<LineRenderer> ();
-
-			teleportArc = GetComponent<TeleportArc> ();
-			teleportArc.traceLayerMask = traceLayerMask;
-
-			float invalidReticleStartingScale = invalidReticleTransform.localScale.x;
-			invalidReticleMinScale *= invalidReticleStartingScale;
-			invalidReticleMaxScale *= invalidReticleStartingScale;
+			ShowPointer (newPointerHand);
 		}
-
-		//-------------------------------------------------
-		void Start ()
+		else if (visible)
 		{
-			HidePointer ();
-
-			player = InteractionSystem.Player.instance;
-
-			if (player == null)
-			{
-				Destroy (this.gameObject);
-				return;
-			}
-		}
-
-		//-------------------------------------------------
-		public void HideTeleportPointer ()
-		{
-			if (pointerHand != null)
+			if (newPointerHand == null && !IsTeleportButtonDown (pointerHand))
 			{
 				HidePointer ();
 			}
-		}
-
-		//-------------------------------------------------
-		void Update ()
-		{
-			Hand newPointerHand = null;
-
-			foreach (Hand hand in player.hands)
+			else if (newPointerHand != null)
 			{
-				if (visible)
-				{
-					if (WasTeleportButtonReleased (hand))
-					{
-					}
-				}
-
-				if (WasTeleportButtonPressed (hand))
-				{
-					newPointerHand = hand;
-				}
-			}
-
-			if (!visible && newPointerHand != null)
-			{
-				//Begin showing the pointer
 				ShowPointer (newPointerHand);
 			}
-			else if (visible)
-			{
-				if (newPointerHand == null && !IsTeleportButtonDown (pointerHand))
-				{
-					//Hide the pointer
-					HidePointer ();
-				}
-				else if (newPointerHand != null)
-				{
-					//Move the pointer to a new hand
-					ShowPointer (newPointerHand);
-				}
-			}
-
-			if (visible)
-			{
-				UpdatePointer ();
-			}
 		}
 
-		//-------------------------------------------------
-		private void UpdatePointer ()
+		if (visible)
 		{
-			Vector3 pointerStart = pointerHand.transform.position;
-			Vector3 pointerEnd;
-			Vector3 pointerDir = pointerHand.transform.forward;
-			bool hitSomething = false;
+			UpdatePointer ();
+		}
+	}
 
-			Vector3 arcVelocity = pointerDir * arcDistance;
+	private void UpdatePointer ()
+	{
+		Vector3 pointerStart = pointerHand.transform.position;
+		Vector3 pointerEnd;
+		Vector3 pointerDir = pointerHand.transform.forward;
+		bool hitSomething = false;
 
-			TeleportMarkerBase hitTeleportMarker = null;
+		Vector3 arcVelocity = pointerDir * arcDistance;
 
-			//Check pointer angle
-			float dotUp = Vector3.Dot (pointerDir, Vector3.up);
-			float dotForward = Vector3.Dot (pointerDir, player.hmdTransform.forward);
-			bool pointerAtBadAngle = false;
-			if ((dotForward > 0 && dotUp > 0.75f) || (dotForward < 0.0f && dotUp > 0.5f))
+		TeleportMarkerBase hitTeleportMarker = null;
+
+		float dotUp = Vector3.Dot (pointerDir, Vector3.up);
+		float dotForward = Vector3.Dot (pointerDir, player.hmdTransform.forward);
+		bool pointerAtBadAngle = false;
+		if ((dotForward > 0 && dotUp > 0.75f) || (dotForward < 0.0f && dotUp > 0.5f))
+		{
+			pointerAtBadAngle = true;
+		}
+
+		RaycastHit hitInfo;
+		teleportArc.SetArcData (pointerStart, arcVelocity, true, pointerAtBadAngle);
+		if (teleportArc.DrawArc (out hitInfo))
+		{
+			hitSomething = true;
+			hitTeleportMarker = hitInfo.collider.GetComponentInParent<TeleportMarkerBase> ();
+		}
+
+		if (pointerAtBadAngle)
+		{
+			hitTeleportMarker = null;
+		}
+
+		if (hitTeleportMarker != null)
+		{
+			if (hitTeleportMarker.locked)
 			{
-				pointerAtBadAngle = true;
-			}
-
-			//Trace to see if the pointer hit anything
-			RaycastHit hitInfo;
-			teleportArc.SetArcData (pointerStart, arcVelocity, true, pointerAtBadAngle);
-			if (teleportArc.DrawArc (out hitInfo))
-			{
-				hitSomething = true;
-				hitTeleportMarker = hitInfo.collider.GetComponentInParent<TeleportMarkerBase> ();
-			}
-
-			if (pointerAtBadAngle)
-			{
-				hitTeleportMarker = null;
-			}
-
-			if (hitTeleportMarker != null) //Hit a teleport marker
-			{
-				if (hitTeleportMarker.locked)
-				{
-					teleportArc.SetColor (pointerLockedColor);
-					pointerLineRenderer.startColor = pointerLockedColor;
-					pointerLineRenderer.endColor = pointerLockedColor;
-					destinationReticleTransform.gameObject.SetActive (false);
-				}
-				else
-				{
-					teleportArc.SetColor (pointerValidColor);
-					pointerLineRenderer.startColor = pointerValidColor;
-					pointerLineRenderer.endColor = pointerValidColor;
-					destinationReticleTransform.gameObject.SetActive (hitTeleportMarker.showReticle);
-				}
-
-				invalidReticleTransform.gameObject.SetActive (false);
-
-				pointedAtPosition = hitInfo.point;
-
-				pointerEnd = hitInfo.point;
-			}
-			else //Hit neither
-			{
+				teleportArc.SetColor (pointerLockedColor);
+				pointerLineRenderer.startColor = pointerLockedColor;
+				pointerLineRenderer.endColor = pointerLockedColor;
 				destinationReticleTransform.gameObject.SetActive (false);
-
-				teleportArc.SetColor (pointerInvalidColor);
-				pointerLineRenderer.startColor = pointerInvalidColor;
-				pointerLineRenderer.endColor = pointerInvalidColor;
-				invalidReticleTransform.gameObject.SetActive (!pointerAtBadAngle);
-
-				//Orient the invalid reticle to the normal of the trace hit point
-				Vector3 normalToUse = hitInfo.normal;
-				float angle = Vector3.Angle (hitInfo.normal, Vector3.up);
-				if (angle < 15.0f)
-				{
-					normalToUse = Vector3.up;
-				}
-				invalidReticleTargetRotation = Quaternion.FromToRotation (Vector3.up, normalToUse);
-				invalidReticleTransform.rotation = Quaternion.Slerp (invalidReticleTransform.rotation, invalidReticleTargetRotation, 0.1f);
-
-				//Scale the invalid reticle based on the distance from the player
-				float distanceFromPlayer = Vector3.Distance (hitInfo.point, player.hmdTransform.position);
-				float invalidReticleCurrentScale = Util.RemapNumberClamped (distanceFromPlayer, invalidReticleMinScaleDistance, invalidReticleMaxScaleDistance, invalidReticleMinScale, invalidReticleMaxScale);
-				invalidReticleScale.x = invalidReticleCurrentScale;
-				invalidReticleScale.y = invalidReticleCurrentScale;
-				invalidReticleScale.z = invalidReticleCurrentScale;
-				invalidReticleTransform.transform.localScale = invalidReticleScale;
-
-				if (hitSomething)
-				{
-					pointerEnd = hitInfo.point;
-				}
-				else
-				{
-					pointerEnd = teleportArc.GetArcPositionAtTime (teleportArc.arcDuration);
-				}
+			}
+			else
+			{
+				teleportArc.SetColor (pointerValidColor);
+				pointerLineRenderer.startColor = pointerValidColor;
+				pointerLineRenderer.endColor = pointerValidColor;
+				destinationReticleTransform.gameObject.SetActive (hitTeleportMarker.showReticle);
 			}
 
-			destinationReticleTransform.position = pointedAtPosition;
-			invalidReticleTransform.position = pointerEnd;
-
-			pointerLineRenderer.SetPosition (0, pointerStart);
-			pointerLineRenderer.SetPosition (1, pointerEnd);
-		}
-
-		//-------------------------------------------------
-		private void HidePointer ()
-		{
-			visible = false;
-
-			teleportArc.Hide ();
-
-			destinationReticleTransform.gameObject.SetActive (false);
 			invalidReticleTransform.gameObject.SetActive (false);
 
-			pointerHand = null;
+			pointedAtPosition = hitInfo.point;
+
+			pointerEnd = hitInfo.point;
+		}
+		else
+		{
+			destinationReticleTransform.gameObject.SetActive (false);
+
+			teleportArc.SetColor (pointerInvalidColor);
+			pointerLineRenderer.startColor = pointerInvalidColor;
+			pointerLineRenderer.endColor = pointerInvalidColor;
+			invalidReticleTransform.gameObject.SetActive (!pointerAtBadAngle);
+
+			Vector3 normalToUse = hitInfo.normal;
+			float angle = Vector3.Angle (hitInfo.normal, Vector3.up);
+			if (angle < 15.0f)
+			{
+				normalToUse = Vector3.up;
+			}
+			invalidReticleTargetRotation = Quaternion.FromToRotation (Vector3.up, normalToUse);
+			invalidReticleTransform.rotation = Quaternion.Slerp (invalidReticleTransform.rotation, invalidReticleTargetRotation, 0.1f);
+
+			float distanceFromPlayer = Vector3.Distance (hitInfo.point, player.hmdTransform.position);
+			float invalidReticleCurrentScale = Util.RemapNumberClamped (distanceFromPlayer, invalidReticleMinScaleDistance, invalidReticleMaxScaleDistance, invalidReticleMinScale, invalidReticleMaxScale);
+			invalidReticleScale.x = invalidReticleCurrentScale;
+			invalidReticleScale.y = invalidReticleCurrentScale;
+			invalidReticleScale.z = invalidReticleCurrentScale;
+			invalidReticleTransform.transform.localScale = invalidReticleScale;
+
+			if (hitSomething)
+			{
+				pointerEnd = hitInfo.point;
+			}
+			else
+			{
+				pointerEnd = teleportArc.GetArcPositionAtTime (teleportArc.arcDuration);
+			}
 		}
 
-		//-------------------------------------------------
-		private void ShowPointer (Hand newPointerHand)
+		destinationReticleTransform.position = pointedAtPosition;
+		invalidReticleTransform.position = pointerEnd;
+
+		pointerLineRenderer.SetPosition (0, pointerStart);
+		pointerLineRenderer.SetPosition (1, pointerEnd);
+	}
+
+	private void HidePointer ()
+	{
+		visible = false;
+
+		teleportArc.Hide ();
+
+		destinationReticleTransform.gameObject.SetActive (false);
+		invalidReticleTransform.gameObject.SetActive (false);
+
+		pointerHand = null;
+	}
+
+	private void ShowPointer (Hand newPointerHand)
+	{
+		if (!visible)
 		{
-			if (!visible)
-			{
-				visible = true;
+			visible = true;
 
-				teleportArc.Show ();
-			}
-
-			pointerHand = newPointerHand;
+			teleportArc.Show ();
 		}
 
-		//-------------------------------------------------
-		public bool IsEligibleForTeleport (Hand hand)
+		pointerHand = newPointerHand;
+	}
+
+	public bool IsEligibleForTeleport (Hand hand)
+	{
+		if (hand == null)
 		{
-			if (hand == null)
-			{
-				return false;
-			}
-
-			if (!hand.gameObject.activeInHierarchy)
-			{
-				return false;
-			}
-
-			if (hand.hoveringInteractable != null)
-			{
-				return false;
-			}
-
-			if (hand.noSteamVRFallbackCamera == null)
-			{
-				if (hand.isActive == false)
-				{
-					return false;
-				}
-			}
-
-			return true;
-		}
-
-		//-------------------------------------------------
-		private bool WasTeleportButtonReleased (Hand hand)
-		{
-			if (IsEligibleForTeleport (hand))
-			{
-				return teleportAction.GetStateUp (hand.handType);
-			}
-
 			return false;
 		}
 
-		//-------------------------------------------------
-		private bool IsTeleportButtonDown (Hand hand)
+		if (!hand.gameObject.activeInHierarchy)
 		{
-			if (IsEligibleForTeleport (hand))
-			{
-				return teleportAction.GetState (hand.handType);
-			}
-
 			return false;
 		}
 
-		//-------------------------------------------------
-		private bool WasTeleportButtonPressed (Hand hand)
-		{
-			if (IsEligibleForTeleport (hand))
-			{
-				return teleportAction.GetStateDown (hand.handType);
-			}
+		return true;
+	}
 
-			return false;
+	private bool WasTeleportButtonReleased (Hand hand)
+	{
+		if (IsEligibleForTeleport (hand))
+		{
+			return teleportAction.GetStateUp (hand.handType);
 		}
+
+		return false;
+	}
+
+	private bool IsTeleportButtonDown (Hand hand)
+	{
+		if (IsEligibleForTeleport (hand))
+		{
+			return teleportAction.GetState (hand.handType);
+		}
+
+		return false;
+	}
+
+	private bool WasTeleportButtonPressed (Hand hand)
+	{
+		if (IsEligibleForTeleport (hand))
+		{
+			return teleportAction.GetStateDown (hand.handType);
+		}
+
+		return false;
 	}
 }
