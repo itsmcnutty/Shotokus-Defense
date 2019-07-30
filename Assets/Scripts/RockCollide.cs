f73bdad91b1a4276d3df0c2a83fc27c6d6aa77ce
William Lucca <wblucca@wpi.edu>
2019-07-30 11:26:28 +0900

    Fix physics damage on new dummies

---
+++ Assets/Scripts/RockCollide.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockCollide : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        int empty = 0;
+    }
+}
