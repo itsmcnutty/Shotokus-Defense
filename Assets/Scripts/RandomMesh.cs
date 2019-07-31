166d881aebcb94fd3cce4680935df31d6ad548e2
William Lucca <wblucca@wpi.edu>
2019-07-31 12:22:27 +0900

    Rock models and complex collisions

--- Assets/Scripts/RandomMesh.cs
+++ Assets/Scripts/RandomMesh.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(SkinnedMeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
 
 public class RandomMesh : MonoBehaviour
 {
     // Array of potential mesh choices
     public Mesh[] meshes;
+    // Array of corresponding material choices
+    public Material[] mats;
     
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,9 @@ public class RandomMesh : MonoBehaviour
         // Pick random Mesh from array
         int meshInd = Random.Range(0, meshes.Length);
         
-        // Set to mesh used in SkinnedMeshRenderer
+        // Set to mesh used in SkinnedMeshRenderer and MeshCollider
         gameObject.GetComponent<SkinnedMeshRenderer>().sharedMesh = meshes[meshInd];
+        gameObject.GetComponent<SkinnedMeshRenderer>().material = mats[meshInd];
+        gameObject.GetComponent<MeshCollider>().sharedMesh = meshes[meshInd];
     }
 }
