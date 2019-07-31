af3bf78dde23251dbc0c1280400a21386cc98403
wblucca <wblucca@wpi.edu>
2019-07-31 09:58:32 +0900

    Add earthbending assets and randomized rock mesh selection

---
+++ Assets/Scripts/RandomMesh.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+
+public class RandomMesh : MonoBehaviour
+{
+    // Array of potential mesh choices
+    public Mesh[] meshes;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Pick random Mesh from array
+        int meshInd = Random.Range(0, meshes.Length);
+        
+        // Set to mesh used in SkinnedMeshRenderer
+        gameObject.GetComponent<SkinnedMeshRenderer>().sharedMesh = meshes[meshInd];
+    }
+}
