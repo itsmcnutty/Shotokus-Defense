d28a6488eed593ace191a2b63312934c328532d8
wblucca <wblucca@wpi.edu>
2019-08-06 21:56:55 +0900

    New animation logic for heavy

--- Assets/Animation/HeavyEnemy/PickRandomAnimation.cs
+++ Assets/Animation/HeavyEnemy/PickRandomAnimation.cs
@@ -7,6 +7,6 @@ public class PickRandomAnimation : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetInteger("AttackNum", Random.Range(0,1));
+        animator.SetInteger("AttackNum", Random.Range(0, 2));
     }
 }
