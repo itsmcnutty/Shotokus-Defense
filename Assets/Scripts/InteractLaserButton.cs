86bd860c053065516eff212191d997721a19f20d
Jose Li <jaliquiel@wpi.edu>
2019-08-08 15:29:54 +0900

    Right hand laser pointer can recognize buttons and clicks. It might only recognize clicks, on and off events. Test more.
    Todo:
    - Investigate more on different ways to interact besides e.target.name()
    - Add all functionality to left hand
    - Make handler for laser an gameobject in the scene
    - look for a possible way to interact with the button, so that the button recognizes that is being clicked
    
    - Make button prefab and instatiate menu when menu button pressed in controller
    - turn off and on, laser pointers
    - beware the slider buttons
    
    Bugs:
    -
    
    Done:
    - Added buttons and colliders
    - add functionality script to button on clicked
    - Button has script that resizes its box collider automatically
    - added automatic button collider script

--- Assets/Scripts/InteractLaserButton.cs
+++ Assets/Scripts/InteractLaserButton.cs
@@ -3,18 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.Extras;
+using UnityEngine.EventSystems;
 
-public class InteractLaseButton : MonoBehaviour
+
+public class InteractLaserButton : MonoBehaviour
 {
 
     public GameObject rightHand;
     private bool selected;
     private SteamVR_LaserPointer laserPointer;
 
+    public EnemyProducer enemyProducer;
+
 
     private void Awake()
     {
         laserPointer = rightHand.GetComponent<SteamVR_LaserPointer>();
+//        laserPointer = gameObject.GetComponent<SteamVR_LaserPointer>();
     }
 
 
@@ -23,6 +28,7 @@ public class InteractLaseButton : MonoBehaviour
     {
         laserPointer.PointerIn += PointerInside;
         laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += OnPointerClick;
         selected = false;
     }
 
@@ -49,6 +55,30 @@ public class InteractLaseButton : MonoBehaviour
             selected = false;
             Debug.Log("pointer is outside this object" + e.target.name);
         }
+    }
+    
+    public void OnPointerClick(object sender, PointerEventArgs e)
+    {
+//        IPointerClickHandler clickHandler = e.target.GetComponent<IPointerClickHandler>();
+//        if (clickHandler == null)
+//        {
+//            return;
+//        }
+// 
+// 
+//        clickHandler.OnPointerClick(new PointerEventData(EventSystem.current));
+        
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was exited");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was clicked");
+            enemyProducer.spawnEnemy(1);
+        }
+        
+        
     }
     
     public bool get_selected_value()
