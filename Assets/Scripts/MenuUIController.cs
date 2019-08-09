d3d26052de84e3107d80ace02be37101b97bf129
Jose Li <jaliquiel@gmail.com>
2019-08-09 16:53:01 +0900

    LaserButton script is implemented for both left and right hand. Also testing steam action event with left and right hand for pause button. Scene changes
    Todo:
    - Investigate more on different ways to interact besides e.target.name()
    - Add all functionality to left hand
    - Make handler for laser an gameobject in the scene
    - look for a possible way to interact with the button, so that the button recognizes that is being clicked
    - Make button prefab and instatiate menu when menu button pressed in controller
    - turn off and on, laser pointers
    - beware the slider buttons
    
    Bugs:
    -
    
    Done:
    - Added buttons and colliders
    - add functionality script to button on clicked
    - Button has script that resizes its box collider automatically
    - added automatic button collider script
    - Right hand laser pointer can recognize buttons and clicks
    - Attempt to make global laser component script, that invokes method from button that is selected

---
+++ Assets/Scripts/MenuUIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class MenuUIController : MonoBehaviour
+{
+    public SteamVR_Input_Sources rightHandInput;
+    public SteamVR_Input_Sources leftHandInput;
+    public SteamVR_Action_Boolean pauseAction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // todo make this toggleable, so if not in menu access it
+        // todo - otherwise, close it
+        if (pausePress())
+        {
+            Debug.Log("Im being PaUsEd");
+        }
+    }
+
+    public bool pausePress()
+    {
+        return pausePressLeft() || pausePressRight();
+    }
+    
+    public bool pausePressRight()
+    {
+        return pauseAction.GetState(rightHandInput);
+    }    
+    
+    public bool pausePressLeft()
+    {
+        return pauseAction.GetState(leftHandInput);
+    }
+}
