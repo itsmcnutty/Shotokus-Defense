2723ca0bf8a2c2309a9144dbdad55fa07b6bdd42
William Lucca <wblucca@wpi.edu>
2019-08-11 12:39:49 +0900

    Fix legs stuck in ground

--- Assets/Scripts/RockCollide.cs
+++ Assets/Scripts/RockCollide.cs
@@ -11,8 +11,5 @@ public class RockCollide : MonoBehaviour
 	private void OnCollisionEnter(Collision other)
 	{
 		crashed = true;
-		GetComponent<Rigidbody>().velocity = Vector3.up;
-		GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-		transform.position = new Vector3(-12, 2, -8.3f);
 	}
 }
