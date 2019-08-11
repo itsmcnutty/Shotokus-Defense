0dcfc982d6cb299dfbbed986b8b13353a4d5e7a3
Jose Li <jaliquiel@gmail.com>
2019-08-11 11:18:41 +0900

    UI Branch Started playing with the instatiation of rock Calculate position of where player looks at LaserButton script is implemented for both left and right hand. Implemented Steam action event with left and right hand for pause button. Scene changes
    Todo:
    - Make handler for laser an gameobject in the scene
    - Instatiate rock when pause button is pressed
    
    - Pause the game when pause menu comes up
    - Investigate more on different ways to interact besides e.target.name()
    - Make button prefab and instatiate menu when menu button pressed in controller
    - turn off and on, laser pointers
    - beware the slider buttons
    - highlight button over hover
    
    Bugs:
    -
    
    Done:
    - Added buttons and colliders
    - add functionality script to button on clicked
    - Button has script that resizes its box collider automatically
    - added automatic button collider script
    - Right hand laser pointer can recognize buttons and clicks
    - Attempt to make global laser component script, that invokes method from button that is selected
    - look for a possible way to interact with the button, so that the button recognizes that is being clicked
    - Add all functionality to left hand

--- Assets/Scripts/MenuUIController.cs
+++ Assets/Scripts/MenuUIController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
@@ -8,7 +9,17 @@ public class MenuUIController : MonoBehaviour
     public SteamVR_Input_Sources rightHandInput;
     public SteamVR_Input_Sources leftHandInput;
     public SteamVR_Action_Boolean pauseAction;
-    
+
+    public GameObject wallPrefab;
+
+    private GameObject player; // get coordinates of player, to instate menu in front of hem
+    private GameObject wall;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("MainCamera");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,26 +29,44 @@ public class MenuUIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // todo make this toggleable, so if not in menu access it
         // todo - otherwise, close it
-        if (pausePress())
+        if (PausePress())
         {
             Debug.Log("Im being PaUsEd");
         }
     }
 
-    public bool pausePress()
+    public bool PausePress()
     {
-        return pausePressLeft() || pausePressRight();
+        return PausePressLeft() || PausePressRight();
     }
     
-    public bool pausePressRight()
+    public bool PausePressRight()
     {
         return pauseAction.GetState(rightHandInput);
     }    
     
-    public bool pausePressLeft()
+    public bool PausePressLeft()
     {
         return pauseAction.GetState(leftHandInput);
     }
+
+    // todo make this toggleable, so if not in menu access it
+    // freezes the game, and instantiates the menu
+    public void PauseGame()
+    {
+
+        Vector3 playerPos = player.transform.position;
+        Quaternion playerRot = player.transform.rotation;
+        Vector3 playerFor = player.transform.forward;
+        Vector3 spawnPosition = playerPos + playerFor*5;
+        
+        
+        wall = Instantiate(wallPrefab, spawnPosition, playerRot);
+        wall.transform.position = new Vector3(5,0,5);
+        
+        
+    }
+    
+    
 }
