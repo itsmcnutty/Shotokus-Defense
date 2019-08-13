3b5541fab77df504aba722ba9606ab60759c64ef
Jose Li <jaliquiel@gmail.com>
2019-08-13 10:26:36 +0900

    Added button scripts and then added them to the pausemenu prefab

---
+++ Assets/Scripts/Button Scripts/SpawnEnemyButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemyButton : MonoBehaviour
+{
+    public int numberOfEnemies;
+    private GameObject enemyProducerObject;
+    private EnemyProducer enemyProducer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyProducerObject = GameObject.FindWithTag("EnemyProducer");
+        enemyProducer = enemyProducerObject.GetComponent<EnemyProducer>();
+        if (!enemyProducer)
+        {
+            Debug.Log("ERROR: Coundnt find the instance ENEMYPRODUCER");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void spawnEnemy()
+    {
+        enemyProducer.spawnEnemy(numberOfEnemies);
+    }
+    
+}
