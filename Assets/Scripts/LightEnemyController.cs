d5ce42c0b66c0b105f3112bfa722b5329af3ea52
Jose Li <jaliquiel@gmail.com>
2019-08-23 13:56:30 +0900

    light enemy moves in navmesh

---
+++ Assets/Scripts/LightEnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Experimental.Rendering;
+
+public class LightEnemyController : MonoBehaviour
+{
+    
+    public NavMeshAgent agent;
+    private GameObject player;
+    private Vector3 playerPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("MainCamera");
+        playerPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+//        playerPos = player.transform.position;
+        agent.SetDestination(playerPos);
+    }
+}
