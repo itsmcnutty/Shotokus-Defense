56a6b5f80fe1c3021837a37be19a46693c4c700d
itsmcnutty <epplevinsky@wpi.edu>
2019-08-26 18:26:52 +0900

    Quicksand slows enemies and rocks

--- Assets/Scripts/Ability Properties/QuicksandProperties.cs
+++ Assets/Scripts/Ability Properties/QuicksandProperties.cs
@@ -1,13 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.AI;
 
 public class QuicksandProperties : MonoBehaviour
 {
     private float quicksandLifetime = 30.0f;
+    private float quicksandSpeedReduction = 3f;
+    private static List<NavMeshAgent> slowedEnemies = new List<NavMeshAgent>();
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,6 +21,30 @@ public class QuicksandProperties : MonoBehaviour
 
     void OnDestroy()
     {
-//        GameObject.FindWithTag("NavMesh").GetComponent<NavMeshSurface>().BuildNavMesh();
+        //        GameObject.FindWithTag("NavMesh").GetComponent<NavMeshSurface>().BuildNavMesh();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        NavMeshAgent agent = other.gameObject.GetComponentInParent<NavMeshAgent>();
+        if (agent != null && !slowedEnemies.Contains(agent))
+        {
+            agent.speed /= quicksandSpeedReduction;
+            slowedEnemies.Add(agent);
+        }
+        else if (other.attachedRigidbody != null)
+        {
+            other.attachedRigidbody.velocity /= quicksandSpeedReduction;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        NavMeshAgent agent = other.gameObject.GetComponentInParent<NavMeshAgent>();
+        if (agent != null && slowedEnemies.Contains(agent))
+        {
+            agent.speed *= quicksandSpeedReduction;
+            slowedEnemies.Remove(agent);
+        }
     }
 }
