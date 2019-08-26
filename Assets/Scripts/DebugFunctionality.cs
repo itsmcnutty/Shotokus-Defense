df8005ea81202541652dfcdb9a357654655f298c
Jose Li <jaliquiel@gmail.com>
2019-08-26 16:32:57 +0900

    merged with master with the mesh render changes. Testing needed for navmeshlinks

---
+++ Assets/Scripts/DebugFunctionality.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugFunctionality : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void takeDamage()
+    {
+        player.GetComponentInChildren<PlayerHealth>().TakeDamage(100);
+    }
+
+    public void togglePowerUps()
+    {
+        PlayerAbility.ToggleRockCluster();
+        PlayerAbility.ToggleSpikeChain();
+        PlayerAbility.ToggleWallPush();
+    }
+    
+}
