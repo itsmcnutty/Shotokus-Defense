6aafec4627a29c1b47431a77dfc777977b58510d
itsmcnutty <epplevinsky@wpi.edu>
2019-08-27 10:02:16 +0900

    Earthquake slows enemies from a distance - still needs some tweaking

--- Assets/Scripts/Ability Properties/QuicksandProperties.cs
+++ Assets/Scripts/Ability Properties/QuicksandProperties.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,11 +8,40 @@ public class QuicksandProperties : MonoBehaviour
 {
     private float quicksandLifetime = 30.0f;
     private float quicksandSpeedReduction = 3f;
+    private float maxEarthquakeDistance;
+    private float earthquakeDuration;
     private static List<NavMeshAgent> slowedEnemies = new List<NavMeshAgent>();
+
+    public static void CreateComponent(GameObject quicksand, float maxEarthquakeDistance, float earthquakeDuration)
+    {
+        QuicksandProperties properties = quicksand.AddComponent<QuicksandProperties>();
+        properties.maxEarthquakeDistance = maxEarthquakeDistance;
+        properties.earthquakeDuration = earthquakeDuration;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-
+        if (PlayerAbility.EarthquakeEnabled())
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemies)
+            {
+                NavMeshAgent agent = enemy.GetComponent<NavMeshAgent>();
+                if (agent != null)
+                {
+                    Debug.Log(agent.name);
+                    float distanceToEarthquake = (enemy.transform.position - gameObject.transform.position).magnitude;
+                    if (distanceToEarthquake < maxEarthquakeDistance && !slowedEnemies.Contains(agent))
+                    {
+                        slowedEnemies.Add(agent);
+                        float slowRate = distanceToEarthquake / maxEarthquakeDistance;
+                        slowRate = (slowRate != 0) ? (float) Math.Pow(slowRate, 1) : float.MinValue;
+                        StartCoroutine(SlowEnemyForTime(agent, slowRate, earthquakeDuration));
+                    }
+                }
+            }
+        }
     }
 
     // Update is called once per frame
@@ -46,5 +77,21 @@ public class QuicksandProperties : MonoBehaviour
             agent.speed *= quicksandSpeedReduction;
             slowedEnemies.Remove(agent);
         }
+    }
+
+    private IEnumerator SlowEnemyForTime(NavMeshAgent agent, float slowRate, float duration)
+    {
+
+        Debug.Log("Before slow: " + agent.speed);
+        agent.speed *= slowRate;
+        Debug.Log("After slow: " + agent.speed);
+        yield return new WaitForSeconds(duration);
+        if (agent != null)
+        {
+            agent.speed /= slowRate;
+            Debug.Log("Finished slow: " + agent.speed);
+        }
+        slowedEnemies.Remove(agent);
+        Debug.Log("NumSlowed=" + slowedEnemies.Count);
     }
 }
