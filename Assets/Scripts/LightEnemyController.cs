947fedb2428941b4a09306732f85a83a657c6a79
wblucca <wblucca@wpi.edu>
2019-09-03 20:24:12 +0900

    Add some new character meshes and experiments with cloth

--- Assets/Scripts/LightEnemyController.cs
+++ Assets/Scripts/LightEnemyController.cs
@@ -11,7 +11,7 @@ public class LightEnemyController : MonoBehaviour
     public NavMeshAgent agent;
     private GameObject player;
     private Vector3 playerPos;
-    
+
     // Start is called before the first frame update
     void Start()
     {
