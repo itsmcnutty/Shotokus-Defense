eb0deacbfee59ba17643a511e665f2c39834a1c6
itsmcnutty <epplevinsky@wpi.edu>
2019-09-18 15:48:57 +0900

    Fixed formatting and added 10 second pauses between waves. The game will also pause at the end of a location and unpause following teleport

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,10 +8,10 @@ public class GameController : MonoBehaviour
     private EnemyProducer enemyProducer; // EnemyProducer script functionality
     private PlayerHealth playerHealth; // controller for player health once round ends
 
-//    public int numOfEnemiesPerWave; // number of enemies to be spawned in one wave 
+    // public int numOfEnemiesPerWave; // number of enemies to be spawned in one wave 
     private int enemiesAlive; // number of enemies alive in current Wave
 
-    [Header("Wave Files")] 
+    [Header("Wave Files")]
     public TextAsset location1WaveFile;
 
     // variables for teleport function
@@ -21,7 +21,7 @@ public class GameController : MonoBehaviour
     private GameObject player; // vrCamera reference, contains all player scripts
     private GameObject vrCamera; // referenced as our player, contains player scripts
     private GameObject cameraRig; // this is the steamVRObjects object 
-    
+
     private GameObject UIControllerObj;
     private GameOverMenuController gameOverController;
 
@@ -31,10 +31,11 @@ public class GameController : MonoBehaviour
     private Wave currentWave;
 
     private float currentTime;
-    
+    private bool pauseWaveSystem;
+
     // Constructor
-    private GameController(){}
-    
+    private GameController() { }
+
     // Instance getter and initialization
     public static GameController Instance
     {
@@ -48,7 +49,6 @@ public class GameController : MonoBehaviour
         }
     }
 
-
     private void Awake()
     {
         // teleport script
@@ -56,23 +56,22 @@ public class GameController : MonoBehaviour
         playerObj = GameObject.FindGameObjectWithTag("Player");
         vrCamera = GameObject.FindGameObjectWithTag("MainCamera");
         cameraRig = GameObject.FindGameObjectWithTag("CameraRig");
-        
-        
-        player = GameObject.FindWithTag ("MainCamera");
+
+        player = GameObject.FindWithTag("MainCamera");
         if (player != null)
         {
-            playerHealth = player.GetComponent<PlayerHealth> ();
+            playerHealth = player.GetComponent<PlayerHealth>();
             // todo maybe restart enemy energy when restarting game
-//            playerEnergy = player.GetComponent<PlayerEnergy> ();
+            //            playerEnergy = player.GetComponent<PlayerEnergy> ();
         }
 
         // todo fix this for multiple producers
         enemyProducerObject = GameObject.FindWithTag("EnemyProducer");
         enemyProducer = enemyProducerObject.GetComponent<EnemyProducer>();
-        
+
         UIControllerObj = GameObject.FindWithTag("UIController");
         gameOverController = UIControllerObj.GetComponent<GameOverMenuController>();
-            
+
         // Parse json to get waves information
         // todo do this for every Location
         allLocationWaves.Enqueue(JsonParser.parseJson(location1WaveFile));
@@ -90,13 +89,18 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (pauseWaveSystem)
+        {
+            return;
+        }
+
         currentTime += Time.deltaTime;
-        if(currentWave != null)
+        if (currentWave != null)
         {
             SpawnInfo spawnInfo = currentWave.GetSpawnAtTime(currentTime);
             if (spawnInfo != null && spawnInfo.Location != SpawnInfo.SpawnLocation.None)
             {
-                enemyProducer.Spawn(spawnInfo);      
+                enemyProducer.Spawn(spawnInfo);
             }
         }
     }
@@ -104,11 +108,11 @@ public class GameController : MonoBehaviour
     // This function starts a round and spawns the corresponding number of enemies
     // Future: this function should keep track of which types enemies to spawn and how many
     // Future: this function should keep track of the round number
-//    void StartWave(SpawnInfo spawnInfo)
-//    {
-//        enemyProducer.Spawn(spawnInfo);
-//    }
-    
+    //    void StartWave(SpawnInfo spawnInfo)
+    //    {
+    //        enemyProducer.Spawn(spawnInfo);
+    //    }
+
     // This function will be called when the player eliminates all the enemies in the wave
     // It starts a new wave, while incrementing the number of enemies that will appear
     // this function should be called everytime an enemy dies
@@ -119,7 +123,7 @@ public class GameController : MonoBehaviour
             // not all enemies have been destroyed, so don't do anything
             return;
         }
-        
+
         SpawnInfo spawnInfo = currentWave.GetNextSpawnTimeInfo(out float? newTime);
         if (spawnInfo != null)
         {
@@ -134,16 +138,17 @@ public class GameController : MonoBehaviour
 
         if (currentWave != null)
         {
-            Debug.Log("Starting next Wave!!");
-            // todo add delay between wave spawning
+            TogglePauseWaveSystem();
+            Invoke("TogglePauseWaveSystem", 10);
             return;
         }
 
         // if there are no waves left, check if there are more locations
-        if(allLocationWaves.Count != 0)
+        if (allLocationWaves.Count != 0)
         {
             currentLocation = resetLocation = allLocationWaves.Dequeue();
             currentWave = currentLocation.GetNextWave();
+            TogglePauseWaveSystem();
             return;
         }
         Debug.Log("YOU WIN");
@@ -161,24 +166,24 @@ public class GameController : MonoBehaviour
         {
             Destroy(enemy);
         }
-        
+
         // destroy menu screens and unfreeze game 
         var menus = GameObject.FindGameObjectsWithTag("Menu");
         foreach (var menu in menus)
         {
             Destroy(menu);
         }
-        
+
         // todo translate player to beginning position
-        
+
         Debug.Log("Restarting game");
-        
+
         // Reset values of wave
         enemiesAlive = 0;
         currentLocation = resetLocation;
         playerHealth.RecoverAllHealth();
     }
-    
+
     // This function is called when player looses
     // It will instantiate the game over menu and give the option to restart the game
     public void playerLost()
@@ -190,7 +195,7 @@ public class GameController : MonoBehaviour
     // To be called when an enemey is destroyed
     public void EnemyGotDestroyed()
     {
-        enemiesAlive --;
+        enemiesAlive--;
     }
 
     // Called in EnemyProducer, updates number of enemies alive
@@ -201,18 +206,17 @@ public class GameController : MonoBehaviour
 
     // This function moves the player around the 5 wave zones
     // todo update player object position too
-        public void Teleport()
+    public void Teleport()
     {
         Vector3 destinationPos;
         int temp = caseSwitch;
         caseSwitch += 1;
-        
+
         // Get camera rig and head position
-//        Transform cameraRig = SteamVR_Render.Top().origin;
+        // Transform cameraRig = SteamVR_Render.Top().origin;
         Transform cameraRigT = cameraRig.transform;
-//        Vector3 headPosition = SteamVR_Render.Top().head.position;
+        // Vector3 headPosition = SteamVR_Render.Top().head.position;
         Vector3 headPosition = vrCamera.transform.position;
-        
 
         Debug.Log("Teleport!");
         temp = temp % 5;
@@ -221,42 +225,46 @@ public class GameController : MonoBehaviour
         switch (temp)
         {
             case 0:
-//                playerObj.transform.position = new Vector3(9,0.25f,33);
-                destinationPos = new Vector3(9,0.25f,33);
+                // playerObj.transform.position = new Vector3(9,0.25f,33);
+                destinationPos = new Vector3(9, 0.25f, 33);
                 break;
             case 1:
-//                playerObj.transform.position = new Vector3(22.6f,0.25f,18.8f);
-                destinationPos = new Vector3(22.6f,0.5f,18.8f);
+                // playerObj.transform.position = new Vector3(22.6f,0.25f,18.8f);
+                destinationPos = new Vector3(22.6f, 0.5f, 18.8f);
                 break;
             case 2:
-//                playerObj.transform.position = new Vector3(-3f,0.25f,3.1f);
-                destinationPos = new Vector3(-3f,0.75f,3.1f);
+                // playerObj.transform.position = new Vector3(-3f,0.25f,3.1f);
+                destinationPos = new Vector3(-3f, 0.75f, 3.1f);
                 break;
             case 3:
-//                playerObj.transform.position = new Vector3(26,0.25f,-22.8f);
-                destinationPos = new Vector3(26,1f,-22.8f);
-                break;           
+                // playerObj.transform.position = new Vector3(26,0.25f,-22.8f);
+                destinationPos = new Vector3(26, 1f, -22.8f);
+                break;
             case 4:
-//                playerObj.transform.position = new Vector3(-1.5f,0.25f,-31.5f);
-                destinationPos = new Vector3(-1.5f,0.75f,-31.5f);
+                // playerObj.transform.position = new Vector3(-1.5f,0.25f,-31.5f);
+                destinationPos = new Vector3(-1.5f, 0.75f, -31.5f);
                 break;
             default:
-//                playerObj.transform.position = new Vector3(0,0,0);
-                destinationPos = new Vector3(0,0,0);
+                // playerObj.transform.position = new Vector3(0,0,0);
+                destinationPos = new Vector3(0, 0, 0);
                 break;
         }
-        
+
         // Calculate translation
-        Vector3 groundPosition = new Vector3(headPosition.x,cameraRigT.position.y, headPosition.z);
+        Vector3 groundPosition = new Vector3(headPosition.x, cameraRigT.position.y, headPosition.z);
         Vector3 translateVector = destinationPos - groundPosition;
 
         // move
         cameraRigT.position += translateVector;
-//        playerObj.transform.position = destinationPos;
+        //        playerObj.transform.position = destinationPos;
 
+        // Unpause the wave system
+        Invoke("TogglePauseWaveSystem", 10);
     }
-    
-    
-    
-    
+
+    private void TogglePauseWaveSystem()
+    {
+        pauseWaveSystem = !pauseWaveSystem;
+    }
+
 }
